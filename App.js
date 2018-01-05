59b3dbe262ce339baefa68141682f940211ff8ff
Saritha Kamath <sarithakamath24@gmail.com>
2018-01-05 15:22:01 -0800

    feat: Add app feature and style
    Removed the default code and created a Text element to display
    and related style.

--- App.js
+++ App.js
@@ -3,8 +3,10 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
-import React, { Component } from 'react';
+'use strict';
+import React, {
+  Component
+} from 'react';
 import {
   Platform,
   StyleSheet,
@@ -12,46 +14,20 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-export default class App extends Component<{}> {
+export default class App extends Component < {} > {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+    return React.createElement(Text, {
+      style: styles.description
+    }, "Search for houses to buy!");
+  };
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
+  description: {
+    fontSize: 18,
     textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    color: '#656565',
+    marginTop: 65,
   },
 });
