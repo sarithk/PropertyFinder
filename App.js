ad716a36da154548f5bb9aefb07946fa59135966
Saritha Kamath <sarithakamath24@gmail.com>
2018-01-06 15:22:11 -0800

    feat: Add text input and a search button
    Added a text input and a Go button placeholders and related style

--- App.js
+++ App.js
@@ -3,32 +3,21 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-'use strict';
-import React, {
-  Component
-} from 'react';
-import {
-  Platform,
-  StyleSheet,
-  NavigatorIOS,
-} from 'react-native';
-import SearchPage from './SearchPage';
-
-
+"use strict";
+import React, { Component } from "react";
+import { Platform, StyleSheet, NavigatorIOS } from "react-native";
+import SearchPage from "./SearchPage";
 
 //Construct a navigation controller, set the initial route to the SearchPage component
-export default class App extends Component < {} > {
+export default class App extends Component<{}> {
   render() {
-    return ( <
-      NavigatorIOS style = {
-        styles.container
-      }
-      initialRoute = {
-        {
-          title: 'Property Finder',
-          component: SearchPage,
-        }
-      }
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: "Property Finder",
+          component: SearchPage
+        }}
       />
     );
   }
@@ -37,11 +26,12 @@ export default class App extends Component < {} > {
 const styles = StyleSheet.create({
   description: {
     fontSize: 18,
-    textAlign: 'center',
-    color: '#656565',
-    marginTop: 65,
+    textAlign: "center",
+    color: "#656565",
+    marginTop: 65
   },
   container: {
-    flex: 1,
-  },
+    flex: 1
+  }
 });
+
