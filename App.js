d8561057df26a4f28e52c28d4ac99af4b4818cdf
Saritha Kamath <sarithakamath24@gmail.com>
2018-01-06 14:52:10 -0800

    feat: Add navigation controller
    Created a SearchPage component which shows the search description
    text. Added navigation controller, style and the initial route to
    the SearchPage component.

--- App.js
+++ App.js
@@ -11,11 +11,13 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  NavigatorIOS,
 } from 'react-native';
 
 
-export default class App extends Component < {} > {
+//SearchPage component
+class SearchPage extends Component < {} > {
   render() {
     return <Text style = {
       styles.description
@@ -24,11 +26,32 @@ export default class App extends Component < {} > {
   };
 }
 
+//Construct a navigation controller, set the initial route to the SearchPage component
+export default class App extends Component < {} > {
+  render() {
+    return ( <
+      NavigatorIOS style = {
+        styles.container
+      }
+      initialRoute = {
+        {
+          title: 'Property Finder',
+          component: SearchPage,
+        }
+      }
+      />
+    );
+  }
+}
+
 const styles = StyleSheet.create({
   description: {
     fontSize: 18,
     textAlign: 'center',
     color: '#656565',
     marginTop: 65,
+  },
+  container: {
+    flex: 1,
   },
 });
