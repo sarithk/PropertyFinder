01421f1f637881dbb99e700d9df6bf8a51fed98c
Saritha Kamath <sarithakamath24@gmail.com>
2018-01-12 17:28:05 -0800

    feat: Add component state to capture entered text input
    Added component state change to trigger UI update.

--- SearchPage.js
+++ SearchPage.js
@@ -12,7 +12,24 @@ import {
 } from "react-native";
 
 export default class SearchPage extends Component<{}> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: "london"
+    };
+  }
+  _onSearchTextChanged = event => {
+    console.log("_onSearchTextChanged");
+    this.setState({ searchString: event.nativeEvent.text });
+    console.log(
+      "Current: " +
+        this.state.searchString +
+        ", Next: " +
+        event.nativeEvent.text
+    );
+  };
   render() {
+    console.log("SearchPage.render");
     return (
       <View style={styles.container}>
         <Text style={styles.description}>Search for houses to buy!</Text>
@@ -22,6 +39,8 @@ export default class SearchPage extends Component<{}> {
         <View style={styles.flowRight}>
           <TextInput
             style={styles.searchInput}
+            value={this.state.searchString}
+            onChange={this._onSearchTextChanged}
             placeholder="Search via name or postcode"
           />
           <Button onPress={() => {}} color="#48BBEC" title="Go" />
