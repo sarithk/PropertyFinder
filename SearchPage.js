d0abc3825decfd1daf69848fd63da73a4b2bcf99
Saritha Kamath <sarithakamath24@gmail.com>
2018-01-13 09:46:31 -0800

    feat: Add logic to initiate a search
    Initiate search using the entered search string and forming the
    URL for API call with the required parameters. Add a spinner to
    indicate the search is in progress.

--- SearchPage.js
+++ SearchPage.js
@@ -11,11 +11,30 @@ import {
   Image
 } from "react-native";
 
+function urlForQueryAndPage(key, value, pageNumber) {
+  const data = {
+    country: "uk",
+    pretty: "1",
+    encoding: "json",
+    listing_type: "buy",
+    action: "search_listings",
+    page: pageNumber
+  };
+  data[key] = value;
+
+  const querystring = Object.keys(data)
+    .map(key => key + "=" + encodeURIComponent(data[key]))
+    .join("&");
+
+  return "https://api.nestoria.co.uk/api?" + querystring;
+}
+
 export default class SearchPage extends Component<{}> {
   constructor(props) {
     super(props);
     this.state = {
-      searchString: "london"
+      searchString: "london",
+      isLoading: false,
     };
   }
   _onSearchTextChanged = event => {
@@ -28,8 +47,19 @@ export default class SearchPage extends Component<{}> {
         event.nativeEvent.text
     );
   };
+  _executeQuery = query => {
+    console.log(query);
+    this.setState({ isLoading: true });
+  };
+
+  _onSearchPressed = () => {
+    const query = urlForQueryAndPage("place_name", this.state.searchString, 1);
+    this._executeQuery(query);
+  };
   render() {
-    console.log("SearchPage.render");
+    const spinner = this.state.isLoading ? (
+      <ActivityIndicator size="large" />
+    ) : null;
     return (
       <View style={styles.container}>
         <Text style={styles.description}>Search for houses to buy!</Text>
@@ -43,9 +73,10 @@ export default class SearchPage extends Component<{}> {
             onChange={this._onSearchTextChanged}
             placeholder="Search via name or postcode"
           />
-          <Button onPress={() => {}} color="#48BBEC" title="Go" />
+          <Button onPress={this._onSearchPressed} color="#48BBEC" title="Go" />
         </View>
         <Image source={require("./Resources/house.png")} style={styles.image} />
+        {spinner}
       </View>
     );
   }
